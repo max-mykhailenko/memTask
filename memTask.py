1fc4b1bf9a4dc9f5b7a3822c0a4c392cb69cecbe
Макс Михайленко <max.mykhailenko@gmail.com>
2013-02-27 16:48:58 +0200

    back to working version

--- memTask.py
+++ memTask.py
@@ -3,20 +3,17 @@ import sublime
 import sublime_plugin
 import json
 import datetime
+from pprint import pprint
 from collections import defaultdict
 import platform
 
 
 class memTask(sublime_plugin.EventListener):
     def __init__(self):
-        sublime.set_timeout(self.runApp, 5000)
-
-    def runApp(self):
         if not hasattr(self, "setting") is None:
             self.setting = {}
             settings = sublime.load_settings(__name__ + '.sublime-settings')
             self.setting['idle'] = settings.get('idle')
-            self.setting['date_format'] = settings.get('date_format')
 
         if platform.system() == 'Windows':
             self.dirSep = "\\"
@@ -27,7 +24,6 @@ class memTask(sublime_plugin.EventListener):
         self.stopTimer = True
         self.fileName = False
         self.fileView = False
-        self.today = datetime.datetime.now().strftime(self.setting['date_format'])
         self.base = self.ReadBaseFromFile()
 
     def ElapsedTime(self):
@@ -38,15 +34,14 @@ class memTask(sublime_plugin.EventListener):
                 self.stopTimer = True
                 return
             else:
-                if self.fileName is not None:
-                    if self.today + self.dirSep + self.fileName in self.base:
-                        self.base[self.today + self.dirSep + self.fileName]["time"] = int(self.base[self.today + self.dirSep + self.fileName]["time"]) + int(5)
-                        self.WriteBaseToFile(self.base)
-                    else:
-                        self.base[self.today + self.dirSep + self.fileName] = {"time": 5}
-                    self.SetStatus('elapsedTime', 'Elapsed time: ' + str(self.SecToHM(self.base[self.today + self.dirSep + self.fileName]["time"])))
+                if self.fileName in self.base:
+                    self.base[self.fileName]["time"] = int(self.base[self.fileName]["time"]) + int(5)
                     self.WriteBaseToFile(self.base)
-                    sublime.set_timeout(self.ElapsedTime, 5000)
+                else:
+                    self.base[self.fileName] = {"time": 5}
+                self.SetStatus('elapsedTime', 'Elapsed time: ' + str(self.SecToHM(self.base[self.fileName]["time"])))
+                self.WriteBaseToFile(self.base)
+                sublime.set_timeout(self.ElapsedTime, 5000)
         else:
             self.EraseStatus('elapsedTime')
 
@@ -56,7 +51,7 @@ class memTask(sublime_plugin.EventListener):
             self.fileName = view.file_name()
         if self.fileView is False or self.fileView is None:
             self.fileView = view
-        if hasattr(self, 'stopTimer') and self.stopTimer is True:
+        if self.stopTimer is True:
             self.stopTimer = False
             self.ElapsedTime()
 
@@ -100,19 +95,32 @@ class memTask(sublime_plugin.EventListener):
         json_data_file.write(json.dumps(data, indent=4, sort_keys=True))
         json_data_file.close()
 
+MT = memTask()
+
+
+class TimeFileEvents(sublime_plugin.EventListener):
+    def on_close(self, view):
+        print 1
+
 
 class ShowTimeCommand(sublime_plugin.WindowCommand):
     def run(self):
+        # Может стоит файл все же сразу куда нить сохранять
         view = self.window.new_file()
         view.set_syntax_file('Packages/memTask/' + __name__ + '.tmLanguage')
         Tree = lambda: defaultdict(Tree)
         tree = Tree()
         base = MT.ReadBaseFromFile()
 
+        TimeFileEvents()
+
         def treeify(seq):
             ret = {}
             for path in seq:
-                seq[path]['pathArray'] = path.split(MT.dirSep)
+                if platform.system() == 'Windows':
+                    seq[path]['pathArray'] = path.split('\\')
+                else:
+                    seq[path]['pathArray'] = path.split('/')
                 # Не брать файлы с временных папок
                 if 'temp' not in seq[path]['pathArray'] and 'Temp' not in seq[path]['pathArray']:
                     cur = ret
@@ -149,5 +157,3 @@ class ShowTimeCommand(sublime_plugin.WindowCommand):
         edit = view.begin_edit()
         printLine(edit, tree, 0)
         view.set_name("all.time")
-
-MT = memTask()
