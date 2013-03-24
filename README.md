fa25156cef3f22d3c56576db80ddc4ee7a2ba6a7
Макс Михайленко <max.mykhailenko@gmail.com>
2013-03-24 19:49:57 +0200

    version 0.1.2

--- README.md
+++ README.md
@@ -8,12 +8,18 @@ memTask will remember all time which you spent in Sublime Text 2
 Tested on:
   - Windows 8
   - Mac OS X 10.7.5
+  - Mac OS X 10.8.3
 
-WARNING! If you have some problems on other OS please add issues.
+WARNING! Reinstall plugin from package repository.
+If you have some problems on other OS please add issues.
+
+## Plans
+  - add comments to each file in tree
+  - add connector to redmine and other project management applications
 
 ## Usage
   - **mac and windows users**
-    - just pull
+    - install plugin from package repository
     - ctrl+alt+t (ctrl+super+t on mac) will show all time. You can save this file with specific date in some folder, add comments
     - ctrl+shift+p "memTask: Show spent time"
 
--- memTask.py
+++ memTask.py
@@ -40,11 +40,12 @@ class memTask(sublime_plugin.EventListener):
                 fp = self.today + self.dirSep + self.fileName
                 if fp in self.base:
                     self.base[fp]["time"] = int(self.base[fp]["time"]) + int(5)
-                    self.WriteBaseToFile(self.base)
                 else:
-                    self.base[fp] = {"time": 5}
+                    self.base[fp] = {
+                        "time": 5,
+                        "path_divider": self.dirSep
+                    }
                 self.SetStatus('elapsedTime', 'Elapsed time: ' + str(self.SecToHM(self.base[fp]["time"])))
-                self.WriteBaseToFile(self.base)
                 sublime.set_timeout(self.ElapsedTime, 5000)
         else:
             self.EraseStatus('elapsedTime')
@@ -62,6 +63,10 @@ class memTask(sublime_plugin.EventListener):
     def on_activated(self, view):
         self.fileName = view.file_name()
         self.fileView = view
+
+    def on_post_save(self, view):
+        print self.base
+        self.WriteBaseToFile(self.base)
 
     def SetStatus(self, place, phrase):
         self.fileView.set_status(place, phrase)
@@ -100,13 +105,28 @@ class ShowTimeCommand(sublime_plugin.WindowCommand):
     def run(self):
         self.ShowReportVariants()
 
+    def IsDate(self, line):
+        try:
+            datetime.datetime.strptime(line, MT.setting['date_format'])
+            return True
+        except Exception:
+            return False
+
     def treeify(self, seq, removeDate):
         ret = {}
         for path in seq:
-            if platform.system() == 'Windows':
-                seq[path]['pathArray'] = path.split('\\')
-            else:
-                seq[path]['pathArray'] = path.split('/')
+            if not hasattr(seq[path], "path_divider"):
+                if "\\" in path:
+                    seq[path]["path_divider"] = "\\"
+                else:
+                    seq[path]["path_divider"] = "/"
+
+            seq[path]['pathArray'] = path.split(seq[path]["path_divider"])
+
+            # if platform.system() == 'Windows':
+            #     seq[path]['pathArray'] = path.split('\\')
+            # else:
+            #     seq[path]['pathArray'] = path.split('/')
 
             if removeDate:
                 if self.IsDate(seq[path]['pathArray'][0]):
@@ -122,13 +142,6 @@ class ShowTimeCommand(sublime_plugin.WindowCommand):
                     else:
                         cur = cur.setdefault(node, {})
         return ret
-
-    def IsDate(self, line):
-        try:
-            datetime.datetime.strptime(line, MT.setting['date_format'])
-            return True
-        except Exception:
-            return False
 
     def ShowGroupedBy(self, type):
         def printLine(edit, tree, level):
@@ -159,12 +172,10 @@ class ShowTimeCommand(sublime_plugin.WindowCommand):
         view.set_syntax_file('Packages/memTask/' + __name__ + '.tmLanguage')
         Tree = lambda: defaultdict(Tree)
         tree = Tree()
-        base = MT.ReadBaseFromFile()
+        self.base = MT.ReadBaseFromFile()
 
-        if type == 'date':
-            tree = self.treeify(base, False)
-        else:
-            tree = self.treeify(base, True)
+        tree = self.treeify(self.base, False if type == 'date' else True)
+
         edit = view.begin_edit()
         printLine(edit, tree, 0)
         view.insert(edit, view.size(), "\n")
